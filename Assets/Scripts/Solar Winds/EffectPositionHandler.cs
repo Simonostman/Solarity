646361b75cc38ca5fec431e7a7619976466c3332
Lukas Clausson <lukas.clausson@hotmail.com>
2020-03-04 16:24:54 +0100

    Made Winds Look Pretty (Ish)

---
+++ Assets/Scripts/Solar Winds/EffectPositionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class EffectPositionHandler : MonoBehaviour
+{
+    public GameObject effect;
+    public GameObject target;
+
+    private bool stopped = false;
+    private float deathTime = 5.0f;
+    private float deathTimer = 0;
+
+    private void FixedUpdate()
+    {
+        if(stopped)
+        {
+            deathTimer += Time.deltaTime % 60;
+            if(deathTimer > deathTime)
+            {
+                Debug.Log("Destroy");
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void Stop()
+    {
+        effect.GetComponent<VisualEffect>().Stop();
+        stopped = true;
+    }
+}
