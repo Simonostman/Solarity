5fb0a2fd339dbcda2b366349336e660d17f76670
Daniel Michaneck <daniel.michaneck@gmail.com>
2020-03-19 15:17:01 +0100

    Menu Update & Cleanup
    Fixed ability to restart game from main menu, also cleaned up some scripts

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("quit game");
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        GameObject levelLoader = GameObject.FindGameObjectWithTag("Level Loader").gameObject;
+
+        if (levelLoader != null)
+        {
+            levelLoader.GetComponent<LevelLoader>().GoToScene("Main Level 1");
+        }
+        else
+        {
+            Debug.Log("No Level Loader found");
+        }
+    }
+}
